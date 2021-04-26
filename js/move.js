cfce0bb715e59b79d41a114cd38ec416ae135968
Damian Wiśniowski <dkontow@gmail.com>
2021-04-26 22:53:03 +0200

    multiple block

--- js/move.js
+++ js/move.js
@@ -1,30 +1,33 @@
-const div = document.querySelector('div');
-let divX = 150;
-let divY = 150;
-div.style.left = `${divX}px`;
-div.style.top = `${divY}px`;
+const divs = document.querySelectorAll('.move');
+let divX;
+let divY;
 let drawActive = false;
 let insertX;
 let insertY;
-div.addEventListener('mousedown', (e) => {
-    div.style.backgroundColor = 'grey';
-    drawActive = !drawActive;
-    insertX = e.offsetX;
-    insertY = e.offsetY;
-})
-document.addEventListener('mousemove', (e) => {
-    if (drawActive) {
-        divX = e.clientX;
-        divY = e.clientY;
-        div.style.left = `${divX-insertX}px`;
-        div.style.top = `${divY-insertY}px`;
-    }
+let activeDiv;
+let move;
+divs.forEach(div => {
+    div.addEventListener('mousedown', function(e) {
+        this.style.backgroundColor = 'grey';
+        activeDiv = this.getAttribute('data-id')
+        move = document.querySelector(`[data-id='${activeDiv}'] `)
+        drawActive = !drawActive;
+        insertX = e.offsetX;
+        insertY = e.offsetY;
+    })
+    document.addEventListener('mousemove', function(e) {
+        if (drawActive) {
+            divX = e.clientX;
+            divY = e.clientY;
+            move.style.left = `${divX-insertX}px`;
+            move.style.top = `${divY-insertY}px`;
+        }
+    })
     div.ondragstart = (e) => {
         e.preventDefault();
     }
-
-})
-div.addEventListener('mouseup', () => {
-    div.style.backgroundColor = 'black';
-    drawActive = !drawActive;
+    div.addEventListener('mouseup', function() {
+        this.style.backgroundColor = 'black';
+        drawActive = !drawActive;
+    })
 })
